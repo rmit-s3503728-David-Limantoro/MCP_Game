18bdca0a6854b0610e31fbfc2248c520ec755e69
dvdlesher <dvdlesher@gmail.com>
2016-03-28 15:28:36 +1100

    Edited Cell class

--- OOGame/src/Cell.java
+++ OOGame/src/Cell.java
@@ -2,12 +2,20 @@
  */
 
 public class Cell {
-   protected int row;
-   protected int col;
-   boolean gotGold = true;
-   public Cell(int i, int j)
-   {
-	  row = i;
-	  col = j;
-   }
+	protected int row;
+	protected int col;
+	private int energyUsed;
+	boolean gotGold = true;
+
+	public Cell(int i, int j, int eU) {
+		row = i;
+		col = j;
+		energyUsed = eU;
+	}
+
+	public Cell(int i, int j) {
+		row = i;
+		col = j;
+		energyUsed = 2;
+	}
 }
