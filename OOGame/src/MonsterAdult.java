f5ecbf11168fe2c1f2587ec85d70d7d96608cb91
rmit-s3585659-nicole-renner <s3585659@student.rmit.edu.au>
2016-03-28 17:48:35 +1100

    Monster Changes

--- OOGame/src/MonsterAdult.java
+++ OOGame/src/MonsterAdult.java
@@ -1,9 +1,19 @@
 
 public class MonsterAdult extends Monster {
+	
+	private static int adultmovetime = 2;
 
 	public MonsterAdult(Grid g, Player p, int row, int col) throws Exception {
-		super(g, p, row, col);
+		super(g, p, row, col, adultmovetime);
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public int checktime() {
+		return adultmovetime;
+	}
+	
+	public void reproduce () {
+	};
 
 }
